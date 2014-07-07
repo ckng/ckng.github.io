8be198bf4430331a0856577b2489683a8451bb28
Chin Kiong, Ng <ckng@baasbook.com>
2014-07-08 01:00:52 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+
+(function($) {
+
+	// Abba A/B Testing
+  Abba('Draft: Action')
+    .control()
+    .variant('Text: Join Beta', function() {
+    	$('#action a').text("Join Beta");
+    })
+    .variant('Color: carrot', function() {
+    	$('#action a').css({ 'background': '#e67e22', 'border-color': '#e67e22' });
+    	$('#action a:hover').css({ 'background': '#d35400', 'border-color': '#d35400' });
+    })
+    .variant('Color: alizarin', function() {
+    	$('#action a').css({ 'background': '#e74c3c', 'border-color': '#e74c3c' });
+    	$('#action a:hover').css({ 'background': '#c0392b', 'border-color': '#c0392b' });
+    })
+    .variant('Color: amethyst', function() {
+    	$('#action a').css({ 'background': '#9b59b6', 'border-color': '#9b59b6' });
+    	$('#action a:hover').css({ 'background': '#8e44ad', 'border-color': '#8e44ad' });
+    })
+    .start();
+
+  // On successful conversion
+  $('#action a').click(function() {
+  	Abba('Landing: Member CTA', {persist: true}).complete();
+  	window.location = $(this).data('href');
+  })
+
+
+})(jQuery);
